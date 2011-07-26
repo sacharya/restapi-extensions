f8016e424d0c12be08871e54daa8460384106851
Sudarshan Acharya <sudarshan.acharya@rackspace.com>
2011-07-26 12:41:32 -0500

    Initial import

---
+++ api/common-api/src/main/java/org/openstack/atlas/api/resources/LoadBalancersResource.java
@@ -0,0 +1,34 @@
+package org.openstack.atlas.api.resources;
+
+import org.openstack.atlas.docs.loadbalancers.api.v1.LoadBalancer;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static javax.ws.rs.core.MediaType.*;
+
+public class LoadBalancersResource {
+    Logger logger = Logger.getLogger("LoadBalancersResource");
+
+    private Integer accountId;
+    private HttpHeaders requestHeaders;
+
+    @POST
+    @Consumes({APPLICATION_XML, APPLICATION_JSON})
+    public Response createLoadBalancer(LoadBalancer loadBalancer) {
+        logger.log(Level.INFO, "loadbalancer: " + loadBalancer);
+
+        return null;
+    }
+
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+
+    public void setRequestHeaders(HttpHeaders requestHeaders) {
+        this.requestHeaders = requestHeaders;
+    }
+}
