39596682ae5141c45469f3a03e46e42c6edfb9f0
Sudarshan Acharya <sudarshan.acharya@rackspace.com>
2011-07-26 23:28:14 -0500

    Xerces element to string.

--- api/common-api/src/main/java/org/openstack/atlas/api/resources/LoadBalancersResource.java
+++ api/common-api/src/main/java/org/openstack/atlas/api/resources/LoadBalancersResource.java
@@ -4,6 +4,7 @@ import org.openstack.atlas.docs.loadbalancers.api.v1.LoadBalancer;
 import org.openstack.atlas.docs.loadbalancers.api.v1.NetworkItem;
 import org.openstack.atlas.docs.loadbalancers.api.v1.accesslist.AccessList1;
 import org.openstack.atlas.docs.loadbalancers.api.v1.accesslist.NetworkItem1;
+import org.w3c.dom.Element;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.HttpHeaders;
@@ -25,17 +26,27 @@ public class LoadBalancersResource {
     @Consumes({APPLICATION_XML, APPLICATION_JSON})
     public Response createLoadBalancer(LoadBalancer loadBalancer) {
         logger.log(Level.INFO, "loadbalancer: " + loadBalancer);
-/*
+
         List<Object> anies = loadBalancer.getAnies();
         for(Object any : anies) {
             logger.log(Level.INFO, "Class: " + any.getClass());
-            AccessList1 accessList = (AccessList1)any;
-            for (NetworkItem1 networkItem1 : accessList.getNetworkItem1s()) {
-                logger.log(Level.INFO, "Class: " + networkItem1.getClass());
+            if(any instanceof Element) {
+                Element element = (Element)any;
+                String name = element.getFirstChild().getNodeName();
+                String value = element.getFirstChild().getNodeValue();
             }
-        }*/
+        }
         return null;
     }
+
+    public static String translateRealCst(Object o){
+		String ret = "";
+		if(o instanceof Element){
+			Element e = (Element)o;
+			ret = ret + e.getFirstChild().getNodeValue().trim();
+		}
+		return ret;
+	}
 
     public void setAccountId(Integer accountId) {
         this.accountId = accountId;
