a6e578bde268ce9453548a4c216fca7848584e63
Sudarshan Acharya <sudarshan.acharya@rackspace.com>
2011-07-28 17:00:01 -0500

    Refactoring core and extensions into separate subprojects.

---
+++ core/core-api/core-common-api/src/main/java/org/openstack/atlas/api/resources/LoadBalancersResource.java
@@ -0,0 +1,41 @@
+package org.openstack.atlas.api.resources;
+
+import org.openstack.atlas.docs.loadbalancers.api.v1.LoadBalancer;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static javax.ws.rs.core.MediaType.*;
+
+public class LoadBalancersResource {
+    Logger logger = Logger.getLogger("LoadBalancersResource");
+
+    private Integer accountId;
+    private HttpHeaders requestHeaders;
+
+    @POST
+    @Consumes({APPLICATION_XML, APPLICATION_JSON})
+    public Response createLoadBalancer(LoadBalancer loadBalancer) {
+        logger.log(Level.INFO, "loadbalancer: " + loadBalancer);
+
+        loadBalancer.getAnies().clear();
+        loadBalancer.getOtherAttributes().clear();
+        return Response.status(Response.Status.OK).entity(loadBalancer).build();
+    }
+
+    public void test() {
+
+    }
+
+
+    public void setAccountId(Integer accountId) {
+        this.accountId = accountId;
+    }
+
+    public void setRequestHeaders(HttpHeaders requestHeaders) {
+        this.requestHeaders = requestHeaders;
+    }
+}
